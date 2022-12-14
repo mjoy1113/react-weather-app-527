b3205e13efac60752d3214ce1399df6ab9c78bec
mjoy1113 <mjoy1113@gmail.com>
2022-12-13 19:08:16 -0500

    Added link to Github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Morgan Rumsey and is open-sourced on{" "}
+        <a
+          href="https://github.com/mjoy1113/react-weather-app-527.git"
+          target="blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
