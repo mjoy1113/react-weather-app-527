c1b5f1ceac603f621f39448f4b52b66e8be88257
mjoy1113 <mjoy1113@gmail.com>
2022-12-13 19:14:49 -0500

    added h1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Morgan Rumsey and is open-sourced on{" "}
-        <a
-          href="https://github.com/mjoy1113/react-weather-app-527.git"
-          target="blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Morgan Rumsey and is open-sourced on{" "}
+          <a
+            href="https://github.com/mjoy1113/react-weather-app-527.git"
+            target="blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
