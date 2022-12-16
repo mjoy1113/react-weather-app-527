8cc4c7334cf25a600eaf707400f970c62b593e5b
mjoy1113 <mjoy1113@gmail.com>
2022-12-15 20:34:24 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Paris" />
         <footer>
           This project was coded by Morgan Rumsey and is open-sourced on{" "}
           <a
             href="https://github.com/mjoy1113/react-weather-app-527.git"
             target="blank"
+            className="footer"
           >
             Github
           </a>
